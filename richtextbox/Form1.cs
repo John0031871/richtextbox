696d88b5223f8239b1f092f3a78a050356769c81
John Hammond <rc0031871@student.riversidecollege.ac.uk>
2018-10-02 10:57:24 +0100

    textboxes

--- richtextbox/Form1.cs
+++ richtextbox/Form1.cs
@@ -16,5 +16,43 @@ namespace richtextbox
         {
             InitializeComponent();
         }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double d = Convert.ToDouble(textBox1.Text);
+                double cuberoot = Math.Pow(d, 1.0 / 3.0);
+                label1.Text = cuberoot.ToString();
+            }
+            catch
+            {
+                label1.Text = "Not a number";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double d = Convert.ToDouble(textBox1.Text);
+                double cuberoot = Math.(d, );
+                label1.Text = cuberoot.ToString();
+            }
+            catch
+            {
+                label1.Text = "Not a number";
+            }
+        }
     }
 }
